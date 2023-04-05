d85f98d8c2499dab90c1f8136d7e23d3ca26f0a3
ALYSSA KONG <alkong123456@GMAIL.com>
2023-04-05 00:39:56 -0700

    added processed data

--- analysis/scripts/project_functions1.py
+++ analysis/scripts/project_functions1.py
@@ -1,19 +1,7 @@
 import pandas as pd
 #wrapping it in a function
-
-def type_of_music(x):
-    '''
-    Decides what type of music the responder participated in. If they were a composer, instrumentalist, both, or neither
-    '''
-    if (x['Instrumentalist']=="Yes") & (x['Composer']=="Yes"):
-        return "Both"
-    elif x['Instrumentalist']=="Yes": 
-        return "Instrumentalist"  
-    elif x['Composer']=="Yes":
-        return "Composer"
-    else:
-        return "Neither"
     
+
 def load_and_process(url_or_path_to_csv_file):
     def type_of_music(x):
         '''
@@ -27,6 +15,13 @@ def load_and_process(url_or_path_to_csv_file):
             return "Composer"
         else:
             return "Neither"
+    def standardization(x):
+        '''
+        returning the standardized value of Hours per day based on the maximum value, standard deviation, and the mean of the dataset
+        '''
+        return ((x['Hours per day']-3.6030995106035886)/(2.730156903404203)+1)/8.470962736230923*100
+
+
     # Method Chain 1 (Load data and deal with missing data)
 
     df1 = (
